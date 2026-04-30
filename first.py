6f0c1e141bc626c7a74eb922cb63bd82a17e95a5
woosj6102 <wooosj@gmail.com>
2026-04-30 20:17:52 +0900

    first commit

---
+++ first.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import pymysql
+from pymysql.cursors import DictCursor
+
+conn = pymysql.connect(
+    host="localhost",
+    user="root",
+    password="",
+    db="",
+    autocommit=True,
+    cursorclass=DictCursor,
+)
